07b5d3fea4b2094a23ac5cafacf314ebe9ec013e
avisser <visser@gmail.com>
2016-03-18 20:05:57 -0400

    checkpoint

--- Core/Classes/Renderer.cs
+++ Core/Classes/Renderer.cs
@@ -1,50 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
-
 using GenArt.AST;
 
 namespace GenArt.Classes
 {
-    public static class Renderer
+  public static class Renderer
+  {
+    //Render a Drawing
+    public static void Render(DnaDrawing drawing, Graphics g, int scale)
     {
-        //Render a Drawing
-        public static void Render(DnaDrawing drawing,Graphics g,int scale)
-        {
-            g.Clear(Color.Black);
+      g.Clear(Color.Black);
 
-            foreach (DnaPolygon polygon in drawing.Polygons)
-                Render(polygon, g, scale);
-        }
-
-        //Render a polygon
-        private static void Render(DnaPolygon polygon, Graphics g, int scale)
-        {
-            using (Brush brush = GetGdiBrush(polygon.Brush))
-            {
-                Point[] points = GetGdiPoints(polygon.Points, scale);
-                g.FillPolygon(brush,points);
-            }
-        }
-
-        //Convert a list of DnaPoint to a list of System.Drawing.Point's
-        private static Point[] GetGdiPoints(IList<DnaPoint> points,int scale)
-        {
-            Point[] pts = new Point[points.Count];
-            int i = 0;
-            foreach (DnaPoint pt in points)
-            {
-                pts[i++] = new Point(pt.X * scale, pt.Y * scale);
-            }
-            return pts;
-        }
-
-        //Convert a DnaBrush to a System.Drawing.Brush
-        private static Brush GetGdiBrush(DnaBrush b)
-        {
-            return new SolidBrush(Color.FromArgb(b.Alpha, b.Red, b.Green, b.Blue));
-        }
-
-        
+      foreach (DnaPolygon polygon in drawing.Polygons)
+        Render(polygon, g, scale);
     }
+
+    //Render a polygon
+    private static void Render(DnaPolygon polygon, Graphics g, int scale)
+    {
+      var brush = GetGdiBrush(polygon.Brush);
+
+      var points = GetGdiPoints(polygon.Points, scale);
+      g.FillPolygon(brush, points); //(brush, points);
+    }
+
+    //Convert a list of DnaPoint to a list of System.Drawing.Point's
+    private static Point[] GetGdiPoints(IList<DnaPoint> points, int scale)
+    {
+      var pts = new Point[points.Count];
+      int i = 0;
+      foreach (var pt in points)
+      {
+        pts[i++] = new Point() {
+          X = pt.X*scale, 
+          Y = pt.Y*scale
+        };
+      }
+      return pts;
+    }
+
+    //Convert a DnaBrush to a System.Drawing.Brush
+    private static System.Drawing.Brush GetGdiBrush(DnaBrush b)
+    {
+      return new SolidBrush(Color.FromArgb(b.Alpha, b.Red, b.Green, b.Blue));
+    }
+  }
 }
---
+++ Core/Models/DrawingChromosome.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using GenArt.Classes;
+using GeneticSharp.Domain.Chromosomes;
+using Google.Protobuf;
+
+namespace GenArt.Core.Models
+{
+  public class DrawingChromosome : ChromosomeBase
+  {
+    public static int MaxPoints { get; set; }
+    public static int Height { get; set; }
+    public static int Width { get; set; }
+
+    public DrawingChromosome(int length) : base(length)
+    {
+    }
+
+    public override Gene GenerateGene(int geneIndex)
+    {
+      var p = new Polygon {Brush = RandomBrush()};
+      var points = RandomPointCount();
+      for (int i = 0; i < points; i++)
+      {
+        p.Points.Add(RandomPoint());
+      }
+
+      using (var ms = new MemoryStream())
+      {
+        p.WriteTo(new CodedOutputStream(ms));
+        return new Gene(ms);
+      }
+    }
+
+    private PointData RandomPoint()
+    {
+      return new PointData()
+      {
+        X = Tools.GetRandomNumber(0, Height-1),
+        Y = Tools.GetRandomNumber(0, Width-1)
+      };
+    }
+
+    private int RandomPointCount()
+    {
+      return Tools.GetRandomNumber(3, 10);
+    }
+
+    private BrushData RandomBrush()
+    {
+      return new BrushData()
+      {
+        Red = Tools.GetRandomNumber(0, 255),
+        Green = Tools.GetRandomNumber(0, 255),
+        Blue = Tools.GetRandomNumber(0, 255),
+        Alpha = Tools.GetRandomNumber(10, 60)
+      };
+    }
+
+    public override IChromosome CreateNew()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
